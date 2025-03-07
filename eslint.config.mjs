ae47dda384fb989355d6bd59fd97f7fe7c1a96bc
machointossh <machointossh@gmail.com>
2025-03-08 01:39:35 +0900

    Enable '@' alias support (#3);
    - Remove '.js' explicit extensions from imports
    - Replace relative imports with '@' aliases
    - `pnpm run lint` succeeds
    
    under the ./test/ directory.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,18 @@ import {fileURLToPath} from 'node:url'
 
 const gitignorePath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')
 
-export default [includeIgnoreFile(gitignorePath), ...oclif, prettier]
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...oclif,
+  prettier,
+  {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ['tsconfig.json', 'test/tsconfig.json'],
+        },
+      },
+    },
+  },
+]
